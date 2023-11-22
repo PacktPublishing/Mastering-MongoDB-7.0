a6624dff02dcf4f10b6527be0064e5fd182995f9
arekborucki <68437078+arekborucki@users.noreply.github.com>
2023-11-22 19:01:43 +0100

    Update mongodb-ruby-inserts.rb

--- Chapter 5/mongodb-ruby-inserts.rb
+++ Chapter 5/mongodb-ruby-inserts.rb
@@ -1,44 +1,40 @@
-# MongoDB Connection and Document Creation using Ruby
-
-# Add the MongoDB driver to your Gemfile
-# gem "mongo"
-
-# Install the MongoDB driver manually (if not using Bundler)
-# gem install mongo
+# MongoDB Documents Creations with BulkWrite API using Ruby
 
 require 'mongo'
 include Mongo
 
 # Connect to the database
-connection_string = "mongodb://HOST:PORT/DATABASE_NAME" # Replace with your connection details
+# Replace 'mongodb://HOST:PORT/DATABASE_NAME' with your actual connection string
+connection_string = "mongodb://HOST:PORT/DATABASE_NAME"
 client = Mongo::Client.new(connection_string)
-db = client.database
-
-# List collection names and metadata
-puts db.collection_names
-puts db.list_collections
-
-# Assuming you have an instance variable for the 'books' collection
 @collection = client[:books]
 
 # Inserting a single document
-document = {
+single_document = {
   isbn: '101',
   name: 'Mastering MongoDB 7.0',
   price: 30
 }
-result = @collection.insert_one(document)
-puts result.n
+@collection.insert_one(single_document)
 
-# Inserting multiple documents
-documents = [
+# Inserting multiple documents using insert_many
+multiple_documents = [
   { isbn: '102', name: 'MongoDB in 7 years', price: 50 },
   { isbn: '103', name: 'MongoDB for experts', price: 40 }
 ]
-result = @collection.insert_many(documents)
+result = @collection.insert_many(multiple_documents)
 
-# The result is a Mongo::BulkWrite::Result object
-puts result.n
-puts result.inserted_ids
+# Batch Operations using BulkWrite API
+# Preparing a batch of insert operations
+documents_for_bulk_write = [
+  { insert_one: { document: { isbn: '104', name: 'MongoDB Essentials', price: 35 } } },
+  { insert_one: { document: { isbn: '105', name: 'The MongoDB Journey', price: 45 } } }
+]
 
-# Note: Replace the connection_string with actual HOST, PORT, and DATABASE_NAME
+# Performing bulk write operations
+bulk_write_result = @collection.bulk_write(documents_for_bulk_write, ordered: true)
+
+# Outputting the results of the bulk write operation
+puts "Number of documents inserted by bulk write: #{bulk_write_result.inserted_count}"
+
+# Note: Replace the connection string with your actual MongoDB server details.
