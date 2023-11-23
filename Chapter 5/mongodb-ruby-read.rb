0eb15a98cb8ae600b50f30018127f7fd462ca859
arekborucki <68437078+arekborucki@users.noreply.github.com>
2023-11-23 09:38:03 +0100

    Update mongodb-ruby.rb

--- Chapter 5/mongodb-ruby-read.rb
+++ Chapter 5/mongodb-ruby-read.rb
@@ -1,13 +1,20 @@
-# Comprehensive MongoDB Operations with Nested Documents using Ruby
+# MongoDB CRUD Operations using Ruby Driver
 
 require 'mongo'
 include Mongo
 
-# Connect to the database
-# Replace 'mongodb://HOST:PORT/DATABASE_NAME' with your actual connection string
-connection_string = "mongodb://HOST:PORT/DATABASE_NAME"
+# Add the MongoDB Ruby driver to your Gemfile
+# gem 'mongo'
+
+# Install the MongoDB Ruby driver manually (if not using Bundler)
+# gem install mongo
+
+# Connection to MongoDB
+connection_string = "mongodb://HOST:PORT/DATABASE_NAME" # Replace with your connection details
 client = Mongo::Client.new(connection_string)
 @collection = client[:books]
+
+# ---- Creating Documents ----
 
 # Inserting a single document
 single_document = {
@@ -15,34 +22,18 @@ single_document = {
   name: 'Mastering MongoDB 7.0',
   price: 30
 }
-@collection.insert_one(single_document)
-
-# Finding documents with multiple criteria
-@collection.find({ isbn: '101', name: 'Mastering MongoDB 7.0' }).each do |doc|
-  puts doc.inspect
-end
+result = @collection.insert_one(single_document)
+puts "Inserted document ID: #{result.inserted_id}"
 
 # Inserting multiple documents
 multiple_documents = [
   { isbn: '102', name: 'MongoDB in 7 years', price: 50 },
   { isbn: '103', name: 'MongoDB for experts', price: 40 }
 ]
-@collection.insert_many(multiple_documents)
+result = @collection.insert_many(multiple_documents)
+puts "Inserted document IDs: #{result.inserted_ids}"
 
-# Query options example
-@collection.find({ isbn: '101' })
-           .projection(price: 1)
-           .sort(name: -1)
-           .limit(5)
-           .skip(2)
-           .batch_size(10)
-           .each { |doc| puts doc.inspect }
-
-# Document count example
-puts "Total count: #{@collection.count_documents({ isbn: '101' })}"
-puts "Estimated count: #{@collection.estimated_document_count}"
-
-# Handling nested documents
+# Inserting a document with nested structure
 nested_document = {
   isbn: '104',
   name: 'Python and MongoDB',
@@ -51,9 +42,53 @@ nested_document = {
 }
 @collection.insert_one(nested_document)
 
-# Retrieving nested document data
+# ---- Reading Data ----
+
+# Finding documents with specific criteria
+@collection.find({ isbn: '101' }).each do |doc|
+  puts doc.inspect
+end
+
+# Using dot notation to access nested fields
 @collection.find({ 'meta.version' => 'MongoDB 7.0' }).each do |doc|
   puts doc.inspect
 end
 
-# Note: Replace the connection string with your actual MongoDB server details.
+# Using logical AND and OR in queries
+@collection.find({ '$or' => [{ isbn: '101' }, { isbn: '102' }] }).each do |doc|
+  puts doc.inspect
+end
+
+# Example of using query options like sort, limit, and projection
+@collection.find({}).sort(name: 1).limit(5).projection(price: 1).each do |doc|
+  puts doc.inspect
+end
+
+# ---- Updating Data ----
+
+# Updating a single document
+@collection.update_one({ 'isbn' => '101' }, { '$set' => { 'name' => 'Mastering MongoDB 7.0 Updated' } })
+
+# Updating multiple documents
+@collection.update_many({ 'price' => { '$lt' => 50 } }, { '$set' => { 'status' => 'discounted' } })
+
+# ---- Deleting Data ----
+
+# Deleting a single document
+@collection.find({ isbn: '103' }).delete_one
+
+# Deleting multiple documents
+@collection.find({ 'price' => { '$gte' => 30 } }).delete_many
+
+# ---- Batch Operations using BulkWrite API ----
+
+# Example of batch operations
+batch_operations = [
+  { insert_one: { document: { isbn: '105', name: 'New MongoDB Book', price: 45 } } },
+  { update_one: { filter: { isbn: '101' }, update: { '$set' => { 'name' => 'Updated Title' } } } },
+  { delete_one: { filter: { isbn: '102' } } }
+]
+bulk_write_result = @collection.bulk_write(batch_operations, ordered: true)
+puts bulk_write_result.bulk_api_result
+
+# Note: Replace HOST, PORT, and DATABASE_NAME with your actual MongoDB connection details.
