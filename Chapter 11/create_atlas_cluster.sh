12075fe69ffc2c10fce34f5dbab142a50d300498
arekborucki <68437078+arekborucki@users.noreply.github.com>
2023-12-30 21:35:18 +0100

    Create create_atlas_cluster.sh

---
+++ Chapter 11/create_atlas_cluster.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script to create an Atlas cluster using Helm
+
+# This command installs the atlas-deployment from the MongoDB Helm repository into the Kubernetes cluster.
+# It sets up the deployment in the 'my-book' namespace and configures the Atlas project with the necessary credentials and details.
+
+helm install atlas-deployment \
+mongodb/atlas-deployment \
+--namespace=my-book \
+--create-namespace \
+--set project.atlasProjectName='My Project' \
+--set atlas.orgId='<orgid>' \  # Replace with your actual organization ID
+--set atlas.publicApiKey='<publicKey>' \  # Replace with your actual public API key
+--set atlas.privateApiKey='<privateApiKey>'  # Replace with your actual private API key
+
+# Ensure to replace <orgid>, <publicKey>, and <privateApiKey> with your actual Atlas organization ID, 
+# public API key, and private API key respectively.
