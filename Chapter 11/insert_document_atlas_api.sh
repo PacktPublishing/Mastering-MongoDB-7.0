1623a48614988e5aad9d6834935a26f36ee78f03
arekborucki <68437078+arekborucki@users.noreply.github.com>
2023-12-31 21:34:44 +0100

    Update insert_document_atlas_api.sh

--- Chapter 11/insert_document_atlas_api.sh
+++ Chapter 11/insert_document_atlas_api.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 # Script to insert a document using the MongoDB Atlas Data API
 
-# This script uses the curl command to send an HTTPS POST request to the Atlas Data API.
-# It inserts a single document into a specified collection within a MongoDB Atlas database.
-# The API endpoint, database, collection, and document details are specified in the request body.
+# Replace <Your-Atlas-API-Key> with your actual MongoDB Atlas API key.
+# Replace <App-ID> with the App ID of your MongoDB Atlas application. 
 
-# Replace the following placeholders with your actual values before running this script:
-# <Your-Atlas-API-Key>: Your MongoDB Atlas API key.
-# myapp-abcde: The App ID of your MongoDB Atlas application.
-
-curl -s "https://data.mongodb-api.com/app/myapp-abcde/endpoint/data/v1/action/insertOne" \
+curl -s "https://data.mongodb-api.com/app/<App-ID>/endpoint/data/v1/action/insertOne" \
   -X POST \
   -H "Content-Type: application/ejson" \
   -H "Accept: application/json" \
@@ -23,6 +18,6 @@ curl -s "https://data.mongodb-api.com/app/myapp-abcde/endpoint/data/v1/action/in
         }
      }'
 
-# Ensure the data source, database, and collection names accurately reflect your Atlas setup.
+# Modify the data source, database, and collection names to reflect your Atlas setup.
 # The document field should contain the data you want to insert.
-# This script assumes you have curl installed and can make outbound HTTPS requests.
+# Requires curl and ability to make outbound HTTPS requests.
