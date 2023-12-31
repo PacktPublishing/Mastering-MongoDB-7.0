5a3e0fff3b7c21b167e5572cc1e170f6628a73e7
arekborucki <68437078+arekborucki@users.noreply.github.com>
2023-12-31 22:27:58 +0100

    Update create_atlas_cluster.sh

--- Chapter 11/create_atlas_cluster.sh
+++ Chapter 11/create_atlas_cluster.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-# Script to create an Atlas cluster using Helm
 
-# This command installs the atlas-deployment from the MongoDB Helm repository into the Kubernetes cluster.
-# It sets up the deployment in the 'my-book' namespace and configures the Atlas project with the necessary credentials and details.
+# This script sets up an Atlas cluster in a Kubernetes environment
+# using MongoDB Helm charts and the Atlas Kubernetes Operator.
+
+# Step 1: Add MongoDB Helm charts repository
+helm repo add mongodb https://mongodb.github.io/helm-charts
+
+# Step 2: Install Atlas Kubernetes Operator
+helm install atlas-operator --namespace=atlas-operator --create-namespace mongodb/mongodb-atlas-operator
+
+# Step 3: Install Atlas Deployment Helm Chart
+# Ensure to replace <orgid>, <publicKey>, and <privateApiKey> 
+# with your actual Atlas organization ID, public API key, 
+# and private API key respectively before running this script.
 
 helm install atlas-deployment mongodb/atlas-deployment \
 --namespace=my-book \
@@ -12,5 +22,3 @@ helm install atlas-deployment mongodb/atlas-deployment \
 --set atlas.publicApiKey='<publicKey>' \  # Replace with your actual public API key
 --set atlas.privateApiKey='<privateApiKey>'  # Replace with your actual private API key
 
-# Ensure to replace <orgid>, <publicKey>, and <privateApiKey> with your actual Atlas organization ID, 
-# public API key, and private API key respectively.
